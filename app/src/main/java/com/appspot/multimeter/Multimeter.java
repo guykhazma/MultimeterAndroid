f39e3735ee3e54534d804dc7dbb8f45d57d772b1
guykhazma <guykhazma@mail.tau.ac.il>
2018-02-21 22:02:40 +0200

    Multimeter start

--- app/src/main/java/com/appspot/multimeter/Multimeter.java
+++ app/src/main/java/com/appspot/multimeter/Multimeter.java
@@ -3,14 +3,20 @@ package com.appspot.multimeter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import it.beppi.knoblibrary.Knob;
+
 public class Multimeter extends AppCompatActivity {
 
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
 
+    private Knob knob;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multimeter);
+
+        knob = (Knob) findViewById(R.id.knob);
     }
 }
